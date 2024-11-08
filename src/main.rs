dddcef21e95bc213e8731cb7267925a281b984d0
inyourface3445 <trenten46@gmail.com>
2024-11-07 22:52:36 -0500

    fin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use clap::Parser;
+use regex::Regex;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    regex_str: String,
+
+    #[arg(short='n', long)]
+    rename: String,
+    
+    #[arg(short, long)]
+    test: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let filter = match Regex::new(&args.regex_str) {
+        Ok(ok) => ok,
+        Err(err) => {
+            println!("{}", err.to_string());
+            std::process::exit(1)
+        }
+    };
+
+    let files: Vec<String> = fs::read_dir("./")
+        .expect("cannot read directory")
+        .map(|n| n.unwrap().path().display().to_string())
+        .filter(|name| filter.is_match(&name))
+        .collect();
+
+    if args.test {
+        for name in files {
+            let name = name.replace("./", "");
+            println!("{name} -> {}", filter.replace(&name, &args.rename));
+        }
+    } else {
+        for name in files {
+            let name = name.replace("./", "");
+            fs::rename(&name, filter.replace_all(&name, &args.rename).to_string()).expect("cannot rename");
+        }
+    }
 }
